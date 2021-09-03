f058a14a48444619532c546614b7392d731f79e2
Jasmyn Hicks <jasmynh44@gmail.com>
2021-09-03 16:29:18 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,16 @@
+var movie = "My Dog Skip"
+
+var isEntertaining = true
+
+movie = "Wonder"
+
+var num4 = 4
+
+var num6 = 6
+
+var product = num4 * num6
+
+var favoriteFoods = ["cookies", "peaches", "wine"]
+favoriteFoods.shift()
+
+movie.length
