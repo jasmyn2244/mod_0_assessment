9a25e43aa0d8cb749d52a87a0621b53082febd5f
Jasmyn Hicks <jasmynh44@gmail.com>
2021-09-03 16:42:19 -0600

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: "T-Rex",
+  eats: "meat"
+  hightInFeet: 22,
+  lifespanInYears: 108,
+  hasShortArms: true,
+  isFeared: true,
+  commonFirstNames: ["Sexy Rexy", "Spike", "Rexercise"]
+  hobbies: ["chomping", "hunting", "roller blading"]
+}
