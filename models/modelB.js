121af0cc5877a478c5be0adf2a89a6ccb940623c
Jasmyn Hicks <jasmynh44@gmail.com>
2021-09-03 16:47:59 -0600

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Gertrude",
+  computer: "MacBook Pro",
+  age: 13,
+  yearsOfExperience: 2,
+  answersToHeyNerd: true,
+  wearsGlasses: false
+  languages: ["Ruby, "Python"],
+  placesWorked: ["Google", "Work Day", "Amazon"],
+}
